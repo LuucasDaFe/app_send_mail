8a06557d9adca13fb1a04f6a6314c3e94cde9699
Lucas Abraao <Labraao957@gmail.com>
2023-06-25 16:53:32 -0300

    Implementando classe mensagem e criando método para validação.

--- processa_envio.php
+++ processa_envio.php
@@ -1,3 +1,38 @@
 <?php
 
-    print_r($_POST);
+    //print_r($_POST);
+    class Mensagem {
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($atributto){
+            return $this->atributo;
+        }
+        public function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
+        public function mensagemValida(){
+            if(empty($this->para) || empty($this->assunto) || empty($this->mensagem)){
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    $mensagem = new Mensagem();
+
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
+
+    echo '<pre>';
+    //print_r($mensagem);
+    echo '</pre>';
+
+    if($mensagem->mensagemValida()){
+        echo 'Mensage é válida';
+    } else{
+        echo 'Mensagem não é válida';
+    };
